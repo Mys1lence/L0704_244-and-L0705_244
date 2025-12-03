5a3ff4e4f13e48a90fe2bfb871647a3b316673f3
AKineva <kuneva695@gmail.com>
2025-12-04 01:07:14 +0500

    Update L0704_244.cpp

--- L0704_244/L0704_244.cpp
+++ L0704_244/L0704_244.cpp
@@ -2,7 +2,7 @@
 
 void insert_edge(Graph* g, int x, int y, bool directed) {
     EdgeNode* p = new EdgeNode;
-    p->weight = 0; // ваш исходный код
+    p->weight = 0;
     p->y = y;
     p->next = g->edges[x];
     g->edges[x] = p;
